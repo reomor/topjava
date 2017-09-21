92403028edb79287de4d349f7238d9c9eac09d87
Rimskiy <rimsk-i@ya.ru>
2017-09-21 19:11:02 +0300

    start

--- src/main/java/ru/javawebinar/topjava/model/UserMeal.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMeal.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
@@ -21,6 +22,10 @@ public class UserMeal {
 
     public LocalDateTime getDateTime() {
         return dateTime;
+    }
+
+    public LocalDate getLocalDate() {
+        return dateTime.toLocalDate();
     }
 
     public String getDescription() {
--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,12 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * GKislin
@@ -23,13 +24,32 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+        List<UserMealWithExceed> list = getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
 //        .toLocalDate();
 //        .toLocalTime();
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        return null;
+        // подсчитываем в мап день - количество каллорий за день
+        Map<LocalDate, Integer> mapSumCaloriesPerDay = mealList
+                .stream()
+                // группировка по дате с подсчетом поля каллорий
+                .collect(Collectors.groupingBy(UserMeal::getLocalDate, Collectors.summingInt(UserMeal::getCalories)));
+        // формирование списка объектов нового типа UserMealWithExceed в соответствии с условием выбора
+        List<UserMealWithExceed> mealWithExceedsList = mealList
+                .stream()
+                // фильтрация записей согласно временному диапазону
+                .filter(userMeal -> TimeUtil.isBetween(userMeal.getDateTime().toLocalTime(), startTime, endTime))
+                // создание объектов нового типа с дополнительным полем
+                .map(userMeal -> {
+                    UserMealWithExceed userMealWithExceed = new UserMealWithExceed(userMeal.getDateTime(),
+                            userMeal.getDescription(),
+                            userMeal.getCalories(),
+                            // получение калорий за день из ранее сформированного мапа
+                            mapSumCaloriesPerDay.get(userMeal.getLocalDate()) > caloriesPerDay);
+                    return userMealWithExceed;
+                })
+                .collect(Collectors.toList());
+        return mealWithExceedsList;
     }
 }
