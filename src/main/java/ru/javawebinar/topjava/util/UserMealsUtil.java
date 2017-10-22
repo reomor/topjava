f3bd86d08a17fe9a8c05130a7b9dfad1990608c2
Rimskiy <rimsk-i@ya.ru>
2017-10-22 22:43:24 +0300

    HW0 main task

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -28,8 +28,26 @@ public class UserMealsUtil {
 //        .toLocalDate();
 //        .toLocalTime();
     }
+    /*
+    Map<String, Integer> items = new HashMap<>();
+    items.put("A", 10);
+    items.put("B", 20);
+    items.put("C", 30);
+    items.put("D", 40);
+    items.put("E", 50);
+    items.put("F", 60);
 
+    items.forEach((k,v)->System.out.println("Item : " + k + " Count : " + v));
+
+    items.forEach((k,v)->{
+        System.out.println("Item : " + k + " Count : " + v);
+        if("E".equals(k)){
+            System.out.println("Hello E");
+        }
+    });
+     */
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        mealList.forEach();
         return null;
     }
     /*
